7016f9725fb9e0dffd266532d74df7ea53429df9
Steven Gourley <sgourley@blueskylogic.net>
2022-05-29 16:45:27 +0100

    Implemented image rendering extension

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
+
 # vscode-markdown-piclink
+
 Add-in for vscode markdown previewer to link images to a photo library.
+
+## Developer references
+
+- [Extension API](https://code.visualstudio.com/api)
+- [Markdown Extension](https://code.visualstudio.com/api/extension-guides/markdown-extension)
+- [markdown-it design principles](https://github.com/markdown-it/markdown-it/blob/master/docs/architecture.md)
+- [helloworld-minimal-sample](https://github.com/microsoft/vscode-extension-samples/tree/main/helloworld-minimal-sample)
---
+++ extension.js
@@ -0,0 +1,39 @@
+
+var vscode = require('vscode');
+
+function activate(ctx) {
+
+    var key = 'markdown.images.library';
+    var url = vscode.workspace.getConfiguration(key).get('url');
+
+    ctx.subscriptions.push(vscode.workspace.onDidChangeConfiguration(function (e) {
+        if (e.affectsConfiguration(key)) {
+            url = vscode.workspace.getConfiguration(key).get('url');
+            vscode.commands.executeCommand('markdown.preview.refresh');
+        }
+    }));
+
+    return {
+        extendMarkdownIt: function (md) {
+
+            var renderImage = md.renderer.rules.image;
+
+            md.renderer.rules.image = function (tokens, idx, options, env, self) {
+
+                var token = tokens[idx],
+                    i = token.attrIndex('src'),
+                    src = token.attrs[i][1] || '';
+
+                if (url && src.startsWith('!')) {
+                    token.attrs[i][1] = url + src.substr(1);
+                }
+
+                return renderImage(tokens, idx, options, env, self);
+            };
+
+            return md;
+        }
+    };
+}
+
+module.exports = { activate };
